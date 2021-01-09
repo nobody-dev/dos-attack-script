0e7ce9e67dbc264ae3fb648e8a45e27ff2b43db2
Ivan Kudryavtsev <ikudryavcev@sberhealth.ru>
2021-01-09 12:49:26 +0700

    upload project

---
+++ README.md
@@ -0,0 +1,11 @@
+# DOS-Attack-Script.
+
+This is a basic script that you can use to do a Denial-of-service attack.
+You can spoof Your ip adress and you can send packets using different ip adress too.
+
+To Use The script You need to have Python3 pre installed On your computer.
+I would recommend You using linux or mac os for this.
+
+How to Run the Script:
+
+    $ python3 main.py
---
+++ art.py
@@ -0,0 +1,9 @@
+
+print("         +-------------------------------------+")
+print("         |          jan, 9th, 2021             |")
+print("         |  This is a simple DOS attack script |")
+print("         |    https://github.com/nobody-dev    |")
+print("         |      Author: Ivan Kudriavcev        |")
+print("         |            Version: 0.1             |")
+print("         +------------------------------------+ ")
+
